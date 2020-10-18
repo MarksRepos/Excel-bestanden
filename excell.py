fba0fa18b1c6fdaf9d9780a98d5beae2586f626b
Mark de Lange <mark@mailsrv.nl>
2020-10-18 20:13:08 +0200

    Initial commit

---
+++ excell.py
@@ -0,0 +1,53 @@
+import os
+from openpyxl import load_workbook, Workbook
+
+print(os.getcwd())
+
+wb = load_workbook(filename='voorbeeld.xlsx')
+
+ws = wb['Personen']
+
+
+# for cell in ws['B']:
+#     print(cell.value)
+
+# for rij in ws['A1:D11']:
+#     for cell in rij:
+#         print(cell.value)
+
+# Begin bij row 2 (zonder header)
+# for rij in ws.iter_rows(min_row=2):
+#     print(rij)
+
+class Persoon():
+
+    def __init__(self, naam, gewicht, leeftijd, woonplaats):
+        self.naam = naam
+        self.gewicht = gewicht
+        self.leeftijd = leeftijd
+        self.woonplaats = woonplaats
+
+    def is_jarig(self):
+        self.leeftijd = self.leeftijd + 1
+        print(f'{self.naam} is nu {self.leeftijd} jaren oud.')
+
+personen = []
+
+for rij in ws.iter_rows(min_row=2):
+
+    persoon = Persoon(rij[0].value, rij[1].value, rij[2].value, rij[3].value)
+    persoon.is_jarig()
+    personen.append(persoon)
+
+wb = Workbook()
+ws = wb.active
+
+for persoon in personen:
+    ws.append([persoon.naam, persoon.gewicht, persoon.leeftijd, persoon.woonplaats])
+
+wb.save('een jaartje ouder.xlsx')
+
+
+
+
+
